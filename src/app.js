417a5b89a984deed8fb37ae119ddca1a706cbcd1
Noah Kvirkvelia <kvirkvelianoah@gmail.com>
2025-07-07 17:16:52 +0400

    start

---
+++ src/app.js
@@ -0,0 +1,8 @@
+document.getElementById('login').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+
+const personalNumber = document.getElementById('personalNumber');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
